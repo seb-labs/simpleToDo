db4993b2a68a4cf37c96eb8fd1b73f535a37ed61
Seb Labs <293422885+seb-labs@users.noreply.github.com>
2026-06-18 15:21:19 +0200

    Revert todo theme to blue

--- app/src/main/java/de/seb/simpletodo/ui/theme/Theme.kt
+++ app/src/main/java/de/seb/simpletodo/ui/theme/Theme.kt
@@ -35,7 +35,7 @@ private val DarkColorScheme = darkColorScheme(
 @Composable
 fun SimpleToDoTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
-    dynamicColor: Boolean = false,
+    dynamicColor: Boolean = true,
     content: @Composable () -> Unit,
 ) {
     val colorScheme = when {
